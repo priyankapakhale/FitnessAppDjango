cd9408bec47fb37d030176cb872fe0b2e8ea7be9
priyanka <priyanka.pakhale54@gmail.com>
2019-06-17 15:41:39 +0530

    updated database credentials

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 import myapp
 from django.contrib import admin
-from django.urls import path
+from django.urls import url
 from myapp import views
 from django.conf.urls import url
 
