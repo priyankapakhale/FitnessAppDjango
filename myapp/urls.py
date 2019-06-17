b1d65ac9b76d93cf1d45d78c0add89cbf727d02f
priyanka <priyanka.pakhale54@gmail.com>
2019-06-17 17:14:02 +0530

    updated database credentials

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-urlspatterns = [
+urlpatterns = [
     path('add_user/',views.add_user, name='add_user'),
     path('get_user/',views.get_user, name='get_user'),
 
