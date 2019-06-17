bd2bbb5a3707841d079237191c1a5a6ec60d5e99
priyanka <priyanka.pakhale54@gmail.com>
2019-06-17 16:21:53 +0530

    updated database credentials

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import myapp
+import my_app
 from django.contrib import admin
 from django.urls import path
-from myapp import views
+from my_app import views
 from django.conf.urls import url
 
 
