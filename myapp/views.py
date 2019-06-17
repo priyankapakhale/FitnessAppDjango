9cca9a73c4cab974ca77118e7d72a5f77f47c283
priyanka <priyanka.pakhale54@gmail.com>
2019-06-17 09:56:10 +0530

    added addUser and getUser views

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.models import User
 
 # Create your views here.
+def add_user(request, email):
+    user = User(first_name = first_name,last_name = last_name, email = email)
+    user.save()
+
+def get_user(request, email):
+    user = User.objects.get(email = email)
+    return user
