aed0f9ceeffb54d0ae1792e7c5aa99533672633c
priyanka <priyanka.pakhale54@gmail.com>
2019-06-20 10:35:23 +0530

    error fixing

---
+++ myapp/ProfileHelper.py
@@ -0,0 +1,28 @@
+from myapp.models import User, UserDetails, FoodInfo
+
+def addUser(first_name,last_name,email_id):
+    u = User(first_name = first_name,
+            last_name = last_name,
+            email_id = email_id
+             )
+    u.save()
+
+def addUserDetails(user, age, gender, weight, height, bmi, goal_weight):
+    print('adding user details')
+    print("user = ",user)
+    print("age = ",age)
+    print("weight = ",weight)
+    print("height = ",height)
+    print("bmi = ",bmi)
+    print("goal_weight = ",goal_weight)
+
+    id = User.objects.filter(user['pk'])
+    u = UserDetails(user = id[0],
+        age = age,
+        gender = gender,
+        weight = weight,
+        height = height,
+        bmi = bmi,
+        goal_weight = goal_weight
+        )
+    u.save()
