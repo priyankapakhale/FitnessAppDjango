48d7b44016a08cb50ca60ec3a6c8ede162c2f941
priyanka <priyanka.pakhale54@gmail.com>
2019-06-20 10:34:29 +0530

    error fixing

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,7 +1,1 @@
 from django.contrib import admin
-from .models import User, UserDetails, FoodInfo
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(UserDetails)
-admin.site.register(FoodInfo)
