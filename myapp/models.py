9c7b6cf66e855f865008b2036070f79fa6570b3d
priyanka <priyanka.pakhale54@gmail.com>
2019-06-22 04:06:09 +0530

    fixing fb login

--- myapp/models.py
+++ myapp/models.py
@@ -11,8 +11,7 @@ class FoodInfo(models.Model):
 
 class User(models.Model):
     id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    name = models.CharField(max_length=50)
     email_id = models.CharField(max_length=50)
 
 class UserDetails(models.Model):
